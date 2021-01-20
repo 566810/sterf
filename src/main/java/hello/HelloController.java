a4dfab12906e558d52339779042d84446146ec6b
566810 <timranzijn1@gmail.com>
2021-01-20 08:47:37 +0100

    first

---
+++ src/main/java/hello/HelloController.java
@@ -0,0 +1,17 @@
+package hello;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    public ResponseEntity<String> hi() {
+        return new ResponseEntity<String>("FF", HttpStatus.OK);
+
+    }
+}
